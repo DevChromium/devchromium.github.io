1000c4614ff1de727b841a84ffcf9200e2a0c43d
DevChromium <chromium@wynntils.com>
2022-03-16 15:44:44 +0100

    feat(lab4): javascript labo 4

---
+++ js1/labo4/manVanAn/js/index.js
@@ -0,0 +1,9 @@
+const setup = () => {
+
+    const text = "De man van An geeft geen hand aan ambetante verwanten";
+    
+    console.log(text.toLowerCase().indexOf("an"))
+
+}
+
+window.addEventListener('load', setup)
---
+++ js1/labo4/spaties/js/index.js
@@ -0,0 +1,23 @@
+const setup = () => {
+    let submitButton = document.getElementById('submit');
+    submitButton.addEventListener('click', formSubmit);
+}
+
+const formSubmit = (event) => {
+    // Zorgt ervoor dat de pagina niet wordt herladen
+    event.preventDefault();
+
+    let woord = document.getElementById('woord').value;
+    console.log(maakInSpaties(woord));
+}
+
+const maakInSpaties = (inputText) => {
+    let result = ""
+    for (let i = 0; i < inputText.length; i++) {
+        const letter = inputText.charAt(i);
+        result += `${letter} `;
+    }
+    return result;
+}
+
+window.addEventListener("load", setup)
