e73a14ebbb1363f22c406e772883cb2540149e9f
DevChromium <chromium@wynntils.com>
2022-03-23 15:16:05 +0100

    chore(lab3): fix one small bug

--- js1/labo4/manVanAn/js/index.js
+++ js1/labo4/manVanAn/js/index.js
@@ -1,7 +1,7 @@
 const setup = () => {
 
     const text = "De man van An geeft geen hand aan ambetante verwanten";
-    let occurences = text.match(/an/g);
+    let occurences = text.toLowerCase().match(/an/g);
     
     console.log(occurences.length)
     
